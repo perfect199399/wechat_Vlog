7963e3baf7e5b3bb8f3018c6a047031dd6a5b3b8
perfect <mr_perfect@aliyun.com>
2021-09-02 18:15:02 +0800

    存储图片资源至云服务器

--- pages/details/details.js
+++ pages/details/details.js
@@ -6,11 +6,12 @@ Page({
         url:'https://img0.baidu.com/it/u=948602605,1373898221&fm=26&fmt=auto&gp=0.jpg',
       },
       {
-        url:'https://ss0.baidu.com/-Po3dSag_xI4khGko9WTAnF6hhy/zhidao/wh%3D680%2C800/sign=b9b90b4b5c2c11dfde84b7255b174ee8/5366d0160924ab184c4b2d9f3efae6cd7b890b3d.jpg'
+        url:'https://cwag-dingding-app.oss-cn-beijing.aliyuncs.com/huashan1.jpg',
       },
       {
-        url:'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg8.zol.com.cn%2Fbbs%2Fupload%2F19824%2F19823826.jpg&refer=http%3A%2F%2Fimg8.zol.com.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1632553581&t=112d3077d9728670bab0fc6c6e62a3ab',
-      }
+        url:'https://cwag-dingding-app.oss-cn-beijing.aliyuncs.com/huashan.jpg'
+      },
+     
     ],
     indicatorDots: true,
     autoplay: true,
